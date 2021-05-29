f7b61e6ff99777200a8abb12b7f350e2f8655527
Angelot Andriamialy <angelot.andriamialy@gmail.com>
2021-05-29 14:03:22 +0300

    PDO options fetch mode

--- database/pdo.php
+++ database/pdo.php
@@ -1,6 +1,12 @@
 <?php
+    $options = [
+        PDO::ATTR_EMULATE_PREPARES   => false, // turn off emulation mode for "real" prepared statements
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, //turn on errors in the form of exceptions
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ, //make the default fetch be an associative array
+    ];
+
     try {
-        $database = new PDO('mysql:host=localhost;dbname=redesigned-fiesta;charset=utf8mb4', 'root', '');
+        $database = new PDO('mysql:host=localhost;dbname=redesigned-fiesta;charset=utf8mb4', 'root', '', $options);
     } catch(Exception $err) {
         if (isset($err)) {
             throw new Exception($err->getMessage());
