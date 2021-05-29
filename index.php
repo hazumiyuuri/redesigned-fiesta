33557dfdee59d4322ebb1e02a812bf970502e472
Angelot Andriamialy <angelot.andriamialy@gmail.com>
2021-05-29 12:48:16 +0300

    Default layout of the application

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
     require('./database/pdo.php');
-
-    $sql_client = "SELECT * FROM clients";
-
-    $res_client = $database->prepare($sql_client);
-    $res_client->execute();
-    $clients = $res_client->fetchAll();
+    require('./utils/functions.php');
+    require('./ressources/layout.php');
+    
