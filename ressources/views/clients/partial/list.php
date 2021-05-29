d954d3d88de2b9c7fbccaa9a879c5370bf407c44
Angelot Andriamialy <angelot.andriamialy@gmail.com>
2021-05-29 13:20:17 +0300

    Updating clients list with bootstrap table

--- ressources/views/clients/partial/list.php
+++ ressources/views/clients/partial/list.php
@@ -6,13 +6,33 @@
 ?>
 <div class="container mt-4">
     <h2>Liste des clients</h2>
-    <?php
-        if (count($clients) <= 0) {
-            echo 'Vide pour le moment.';
-        }
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Nom</th>
+                <th>Prénom</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                if (count($clients) <= 0) {
+                    echo '  <tr>
+                                <td colspan="4" class="text-center">Vide pour le moment.</td>
+                            </tr>';
+                }
 
-        foreach ($clients as $client) {
-            echo $client->name;
-        }
-    ?>
+                foreach ($clients as $client) {
+                    echo '  <tr>
+                                <td>' . $client->id . '</td>
+                                <td>' . $client->nom . '</td>
+                                <td>' . $client->prenom . '</td>
+                                <td>' . $client->email . '</td>
+                            </tr>';
+                }
+            ?>
+        </tbody>
+    </table>
+    
 </div>
