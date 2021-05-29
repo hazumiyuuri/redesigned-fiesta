2e14efe2577753b6a61d633f9c3bb8213aa332d1
Angelot Andriamialy <angelot.andriamialy@gmail.com>
2021-05-29 19:38:55 +0300

    Client action form update

--- ressources/views/clients/partial/edit.php
+++ ressources/views/clients/partial/edit.php
@@ -1,10 +1,16 @@
 <?php
-    // Adding new client to database
     $errors = null;
+    $sql_client = "SELECT * FROM clients WHERE id = ?";
+    $res_client = $database->prepare($sql_client);
+    $res_client->execute([
+        $_GET['client_id']
+    ]);
+    $client = $res_client->fetch();
 
+    // Updating existing client to database
     if (isset($_POST['SAVE_CLIENT'])) {
         // Checking sended data if not empty 
-        $check_email = check_email($_POST['email']);
+        $check_email = check_email($_POST['email'], true);
         $check_data = check_data(['nom', 'prenom', 'email']);
 
         if ($check_data) {
@@ -13,18 +19,19 @@
             if ($check_email) {
                 $errors['insert'] = true;
             } else {
-                $sql_client = "INSERT INTO `clients` 
-                                        (`id`, `created_at`, `nom`, `prenom`, `email`) 
-                                    VALUES 
-                                    (NULL, ?, ?, ?, ?)";
+                $sql_client = "UPDATE `clients` 
+                                    SET nom = ?,
+                                        prenom = ?,
+                                        email = ?
+                                    WHERE id = ? LIMIT 1";
     
                 $res_client = $database->prepare($sql_client);
                 $res_client->execute(
                     array(
-                        date('Y-m-d H:i:s'),
                         $_POST['nom'],
                         $_POST['prenom'],
                         $_POST['email'],
+                        $_GET['client_id']
                     )
                 );
     
@@ -36,7 +43,7 @@
 <div class="add_form">
     <form method="post">
         <div class="content container">
-            <h2>Ajout d'un nouveau client</h2>
+            <h2>Modification du client</h2>
             <?php
                 if (isset($errors['insert'])) {
                     echo '<div class="alert alert-danger" role="alert">
@@ -52,17 +59,17 @@
             ?>
             <div class="mb-3">
                 <label class="form-label">Nom</label>
-                <input type="text" name="nom" class="form-control">
+                <input type="text" name="nom" value="<?php echo $client->nom; ?>" class="form-control">
             </div>
             <div class="mb-3">
                 <label class="form-label">Prénom</label>
-                <input type="text" name="prenom" class="form-control">
+                <input type="text" name="prenom" value="<?php echo $client->prenom; ?>" class="form-control">
             </div>
             <div class="mb-3">
                 <label class="form-label">Email</label>
-                <input type="text" name="email" class="form-control">
+                <input type="text" name="email" value="<?php echo $client->email; ?>" class="form-control">
             </div>
-            <button class="btn btn-primary">Ajouter</button>
+            <button class="btn btn-warning">Mettre à jour</button>
             <a href="?pages=clients">
                 <button type="button" class="btn btn-secondary">Annuler</button>
             </a>
--- ressources/views/clients/partial/list.php
+++ ressources/views/clients/partial/list.php
@@ -1,5 +1,5 @@
 <?php
-    $sql_client = "SELECT * FROM clients";
+    $sql_client = "SELECT *, DATE_FORMAT(clients.created_at, \"%d/%m/%Y %h:%i\") AS date_creation FROM clients";
     $res_client = $database->prepare($sql_client);
     $res_client->execute();
     $clients = $res_client->fetchAll();
@@ -13,25 +13,39 @@
         <thead>
             <tr>
                 <th>Id</th>
+                <th>Date de création</th>
                 <th>Nom</th>
                 <th>Prénom</th>
                 <th>Email</th>
+                <th>Action</th>
             </tr>
         </thead>
         <tbody>
             <?php
                 if (count($clients) <= 0) {
                     echo '  <tr>
-                                <td colspan="4" class="text-center">Vide pour le moment.</td>
+                                <td colspan="5" class="text-center">Vide pour le moment.</td>
                             </tr>';
                 }
 
                 foreach ($clients as $client) {
                     echo '  <tr>
                                 <td>' . $client->id . '</td>
+                                <td>' . $client->date_creation . '</td>
                                 <td>' . $client->nom . '</td>
                                 <td>' . $client->prenom . '</td>
                                 <td>' . $client->email . '</td>
+                                <td class="d-flex">
+                                    <form method="post" action="?pages=clients&action=edit_client&client_id=' . $client->id . '">
+                                        <button type="submit" class="btn btn-primary btn-sm">Editer</button>
+                                    </form>
+
+                                    &nbsp;
+
+                                    <form method="post" action="?page=clients&module=detail&client_id=' . $client->id . '">
+                                        <button type="submit" class="btn btn-warning btn-sm">Voir détail</button>
+                                    </form>
+                                </td>
                             </tr>';
                 }
             ?>
