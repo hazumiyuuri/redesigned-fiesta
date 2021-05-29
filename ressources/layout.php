8f2d00e11d1dc390ed267647c8f4bcbaa95d46b3
Angelot Andriamialy <angelot.andriamialy@gmail.com>
2021-05-29 13:15:55 +0300

    Menu integration in layout

--- ressources/layout.php
+++ ressources/layout.php
@@ -6,11 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestion client et voiture</title>
-    <link rel="stylesheet" href="<?php asset('css/bootstrap/boostrap.css') ?>">
+    <link rel="stylesheet" href="<?php asset('css/bootstrap/bootstrap.min.css') ?>">
 </head>
 <body>
     <?php
         // Loading default component
+        load_component('menu');
         load_component();
     ?>
 </body>
