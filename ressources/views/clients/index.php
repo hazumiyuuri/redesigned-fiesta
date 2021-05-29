41fcd3f2293e1b37f11c9b4b729cf3015bd5c90f
Angelot Andriamialy <angelot.andriamialy@gmail.com>
2021-05-29 15:00:12 +0300

    Client index add form partial & check_email method

--- ressources/views/clients/index.php
+++ ressources/views/clients/index.php
@@ -1,4 +1,22 @@
 <?php
     load_component('clients', 'partial.list');
-    load_component('clients', 'partial.add');
+
+    if (isset($_GET['action']) && $_GET['action'] === 'add_client') {
+        load_component('clients', 'partial.add');
+    }
+
+    function check_email($email) {
+        global $database;
+
+
+        $sql_client = "SELECT * FROM clients WHERE email = ? LIMIT 1";
+        $res_client = $database->prepare($sql_client);
+        $res_client->execute([
+            $email
+        ]);
+
+        $client = $res_client->fetch();
+
+        return isset($client->id);
+    }
 ?>
